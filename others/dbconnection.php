a78a5e7627d73c30efee61d836afc1c95705b20f
Sourav Debnath <73267117+souravdn@users.noreply.github.com>
2021-08-06 20:36:22 +0530

    Update dbconnection.php

--- others/dbconnection.php
+++ others/dbconnection.php
@@ -1,19 +1,29 @@
 <?php
 //connect to db
-$con=new mysqli("localhost","root","");
-if(!$con){
-    // echo "db connection failed";
+$servername="mysql-43037-0.cloudclusters.net";
+ $database="neutrify";
+  $username="sourav";
+ $password="Debnath@1";
+// $dname="neutrify";
+$PORT="17303";
+
+
+$con=new mysqli($servername, $username, $password,$dname,$PORT);
+if($con==TRUE){
+//     echo "connected to db";
 }
 else{
-    // echo "connected to db";
+//   echo "db connection failed";
+   
 }
 
 
 //select db
-if($con->query("USE project_shoppingcart")){
-    // echo "db selected";
+if($con->query("USE neutrify")){
+//     echo "db selected";
 }
 else{
-    // echo "db not selected";
+//     echo "db not selected";
 }
+
 ?>
